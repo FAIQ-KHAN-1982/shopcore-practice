1421ed4be312edbb2fe91b95e05ac4176c8f08e1
Muhammad Faiq <faiqkaneki256@gmail.com>
2026-09-05 23:17:10 +0500

    feat: working on categories

--- App/Services/Product_service.py
+++ App/Services/Product_service.py
@@ -2,5 +2,14 @@ from sqlalchemy.orm import Session
 from App.Schemas import CategoryCreate
 
 def AddCategory(data: CategoryCreate, db: Session):
-    pass
+    new_category = CategoryCreate(
+        name = data.name,
+        slug = data.slug,
+        description = data.description,
+        image_url = data.image_url,
+        parent_id = data.parent_id,   
+        sort_order = data.sort_order
+    )
+
+    
     
